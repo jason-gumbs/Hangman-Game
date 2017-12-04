3afc2c8c54e88ae179d2553829e9b41f0a73eaa2
Will <fwgumbs@gmail.com>
2017-12-03 20:21:16 -0500

    new

---
+++ assets/javascript/game.js
@@ -0,0 +1,34 @@
+ var artistList = [
+      "Jayz",
+      "T.I",
+      "Lil Baby",
+      "Young Jeezy",
+      "2 Chainz",
+      "Migos",
+      "Lil Yahty",
+      "Outcast",
+      "K camp"
+    ];
+var  score = "_";
+
+var item = artistList[Math.floor(Math.random()*artistList.length)];
+
+
+
+
+for (var i = 0; i < item.length; i++) {
+
+     
+    document.write(score + " ");
+
+    }
+
+
+ //item.toString();
+//var n = item.replace("item", "_");
+//alert(n);
+
+    //document.onkeyup = function(event) {
+           //var userGuess = event.key.toLowerCase();}
+
+           //console.log("Hiiiriri");
