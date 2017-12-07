7c4606bfd95ccac9098a96f5a77ef38ca83551f3
Will <fwgumbs@gmail.com>
2017-12-07 00:04:03 -0500

    new

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,28 +1,95 @@
- var artistList = [
-      "Jayz",
-      "T.I",
-      "Lil Baby",
-      "Young Jeezy",
-      "2 Chainz",
-      "Migos",
-      "Lil Yahty",
-      "Outcast",
-      "K camp"
-    ];
-var  score = "_";
+var artistList = ["ti","lilbaby","youngjeezy","2chainz","migos","lilyahty","outcast","kcamp"];
+var  namelength =0;
+var chosenArtist = "";
+var item = "";
+var blanks = [];
+var usedKeys = [];
+var correct = 0;
+var j = 10;
+
+document.getElementById("rem").innerHTML = "you have "+ j + " guesses remaining";
 
 var item = artistList[Math.floor(Math.random()*artistList.length)];
 
+chosenArtist = item.split("");
+namelength = chosenArtist.length;
+console.log(chosenArtist);
 
 
 
-for (var i = 0; i < item.length; i++) {
 
+for (var i = 0; i < namelength; i++) {
+      blanks.push("_");
+      
      
-    document.write(score + " ");
+      }
+      console.log(blanks);
+      document.getElementById("question").innerHTML = blanks.join(" ");
+     
 
+
+document.onkeyup = function(event) {
+     
+    var keypressed =String.fromCharCode(event.keyCode).toLowerCase()
+    usedKeys.push(keypressed);
+
+      document.getElementById("myList").innerHTML = usedKeys.join(" ");;
+   
+     
+    console.log(keypressed);
+
+    var keyguess = false;
+    if (item[i] !== correct){
+      youWon();
     }
+    
 
+
+
+
+
+      for (var i = 0; i < blanks.length; i++) {
+            if (item[i] == keypressed){
+                  keyguess = true;
+                  blanks[i] = keypressed;
+                  correct++
+                  document.getElementById("question").innerHTML = blanks.join(" ");
+                  console.log(correct);
+
+            }
+            
+
+}
+
+
+};
+
+
+
+
+function wrongkey(){
+     
+
+   j--
+      document.getElementById("rem").innerHTML = "you have "+ j + " guesses remaining";
+   if (j==0){
+        document.write("Game Over");
+   }
+      
+     
+};
+
+function youWon(){
+     
+       document.write("You win");
+};
+
+
+youWon();
+
+
+
+//    return a * b;                // Function returns the product of a and b
 
  //item.toString();
 //var n = item.replace("item", "_");
